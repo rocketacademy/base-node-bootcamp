df79ed4f39e3a9c704e1b4bdcc658ef2d3bd51bb
kennethongcs <kenneth.ongcs@gmail.com>
2022-01-18 11:08:35 +0800

    moved func involved with conversions

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 import { writeFile, readFile } from 'fs';
+import {
+  convertHexToRGB,
+  componentToHex,
+  rgbToHex,
+} from './conversionFunctions.js';
 
-const convertHexToRGB = (hex) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
-};
-
-const file = process.argv[2];
+const file = process.argv[3];
+const convertFunction = process.argv[2];
 
 const addConvertedLines = (content) => {
   // convert file into individual lines
   const lines = content.split('\n');
   // loop through each line and check for hex color code
   let newLine = '';
-  for (let i = 0; i < lines.length; i += 1) {
-    // returns false or indexNo of #
-    const hexIndexNo = checkHexInLine(lines[i]);
-    // console.log(hexIndexNo);
-    if (hexIndexNo) {
-      const hexNo = lines[i].slice(hexIndexNo, lines[i].length - 2);
-      const convertToRGB = convertHexToRGB(hexNo);
-      const { r, g, b } = convertToRGB;
-      newLine += `background-color: rgb(${r}, ${g}, ${b})\n`;
-    }
-    if (!hexIndexNo) {
-      newLine += `${lines[i]}\n`;
+  // convert only HEX to RGB
+  if (convertFunction === 'hextorgb') {
+    for (let i = 0; i < lines.length; i += 1) {
+      // returns false or indexNo of #
+      const hexIndexNo = checkHexInLine(lines[i]);
+      // console.log(hexIndexNo);
+      if (hexIndexNo) {
+        const hexNo = lines[i].slice(hexIndexNo, lines[i].length - 2);
+        const convertToRGB = convertHexToRGB(hexNo);
+        const { r, g, b } = convertToRGB;
+        newLine += `background-color: rgb(${r}, ${g}, ${b})\n`;
+      }
+      if (!hexIndexNo) {
+        newLine += `${lines[i]}\n`;
+      }
     }
   }
+  // convert only RGB to HEX DOING
+  if (convertFunction === 'rgbtohex') {
+  }
+
   return newLine;
 };
 
