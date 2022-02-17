99619fe19955f5d2262c51440fa22283e62ab6cf
R4Y815 <Akcent360@gmail.com>
2022-02-17 19:13:47 +0800

    Meal Logger: Basics to Comfortable 1,2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Client } = pg;
 const pgConnectionConfigs = {
   user: 'raytor27',
   host: 'localhost',
-  database: 'dogs_db',
+  database: 'mealkeeper',
   port: 5432, // Postgres server always runs on this port
 };
 
@@ -30,29 +30,45 @@ const whenQueryDone = (error, result) => {
   client.end();
 };
 
+/* to show db Entries */
+const sqlQuery1 = 'SELECT * FROM meals';
+/* to delete entire table!! */
+const sqlQuery2 = 'DROP TABLE meals';
+/* create table header for meal logging */
+const sqlQuery3 = 'CREATE TABLE meals (id SERIAL PRIMARY KEY, meal_type TEXT, description TEXT, amt_of__alcohol_mL INTEGER, was_hungry_b4_eating BOOLEAN )';
+/* Inserting meal logs */
+const sqlQuery4 = `INSERT INTO meals (meal_type, description, amt_of__alcohol_mL, was_hungry_b4_eating ) VALUES ('${process.argv[3]}', '${process.argv[4]}', '${process.argv[5]}', '${process.argv[6]}' )`;
+/* to show table in customised format in bash terminal */
+const sqlQuery1B = 'SELECT * FROM meals';
+/* edit entry node index.js <command(2)> <id(3)> <Col Name(4)> <New Value(5)> !! only 1 cell at a time */
+const editSqlQuery = `UPDATE meals SET ${process.argv[4]} = '${process.argv[5]}' WHERE id=${process.argv[3]}`
+const sqlQuery0 = 'SELECT * FROM meals';
+
 const command = process.argv[2];
-switch(command) {
+switch (command) {
   case 'show':
-    const sqlQuery1 = 'SELECT * FROM dogs';
     // run the SQL query
     client.query(sqlQuery1, whenQueryDone);
     break;
-  case 'all-dogs':
-    const sqlQuery1B = 'SELECT * FROM dogs';
+  case 'dropTable':
+    client.query(sqlQuery2, whenQueryDone);
+    break;
+  case 'create':
+    client.query(sqlQuery3, whenQueryDone);
+    break;
+  case 'log':
+    client.query(sqlQuery4, whenQueryDone);
+    break;
+  case 'edit':
     // run the SQL query
+    client.query(editSqlQuery, whenQueryDone);
+  break;
+  case 'report':
+    // run the SQL query
+    console.log(typeof(process.argv[2]));
     client.query(sqlQuery1B, whenQueryDone);
     break;
-  case 'dropTable':
-    const sqlQuery2 = 'DROP TABLE dogs';
-    client.query(sqlQuery2, whenQueryDone);
-  case 'create':
-    const sqlQuery3 = 'CREATE TABLE dogs (id SERIAL PRIMARY KEY, name TEXT, type TEXT, weight INTEGER )';
-    client.query(sqlQuery3, whenQueryDone);
-  case 'insert':
-    const sqlQuery4 = `INSERT INTO dogs (name, type, weight) VALUES ('${process.argv[3]}', '${process.argv[4]}', '${process.argv[5]}' )`;
-    client.query(sqlQuery4, whenQueryDone); 
   default:
-    const sqlQuery0 = 'SELECT * FROM dogs';
     // run the SQL query
     client.query(sqlQuery0, whenQueryDone);
     console.log('session end');
