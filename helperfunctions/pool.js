af1e317f1aff6cb18b39d99c860360d348a489a0
berniceyeo <berniceyeosuping@gmail.com>
2022-04-26 20:58:27 +0800

    add heroku sql config

--- helperfunctions/pool.js
+++ helperfunctions/pool.js
@@ -2,13 +2,29 @@ import pg from 'pg';
 
 // LINKING TO RELATED DATABASE
 const { Pool } = pg;
-const pgConnectionConfigs = {
-  user: 'bernice',
-  host: 'localhost',
-  database: 'collab',
-  port: 5432,
-};
 
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: 'bernice',
+    host: 'localhost',
+    database: 'collab',
+    port: 5432,
+  };
+}
 const pool = new Pool(pgConnectionConfigs);
+
+// ...
 
 export default pool;
